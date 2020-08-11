34b9b88c117fe63ebc40205dc8bf68b83c6e665a
AlecApp <69335078+AlecApp@users.noreply.github.com>
2020-08-10 21:08:19 -0400

    Update InstallAnsible.sh

--- InstallAnsible.sh
+++ InstallAnsible.sh
@@ -1,12 +1,11 @@
-#A shell script to automate the initial Ansible install on Red Hat Linux AMI outputs the RSA key to import into the host instances.
+#A shell script to install Ansible on Red Hat Linux AMI
+#takes 1 argument, which is the IPv4 address of the slave client to be added to the Ansible hosts file.
 sudo yum update
 sudo yum -y install python3
 sudo alternatives --set python /usr/bin/python3
 sudo yum -y install python3-pip
 sudo pip3 install ansible --user
-sudo mkdir /etc/ansible
-sudo mkdir ~/.ssh
-sudo echo $1 >> /etc/ansible/hosts
-ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
-cat id_rsa.pub
+sudo mkdir -p /etc/ansible
+echo $1 | sudo tee /etc/ansible/hosts
+ssh-keygen -f .ssh/id_rsa -t rsa -N ''
 echo 'Done'
